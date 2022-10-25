403900b58caeecf209b25e516f669888960f9578
MaxterzzOnCoding <95652868+MaxterzzOnCoding@users.noreply.github.com>
2022-10-25 12:46:37 +0200

    Alter Confirmation
    Programm was checkt ob der User volljährig ist

---
+++ Alter/Alter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Alter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int alter;
+            string alterstring;
+            Console.Write("Bitte geben Sie Ihr Alter ein: ");
+            alterstring = Console.ReadLine();
+            alter = Convert.ToInt32(alterstring);
+            if (alter >= 18)
+            {
+                Console.WriteLine("Sie sind Volljährig");
+            }
+            else
+            {
+                Console.WriteLine("Sie sind nicht Volljährig");
+                Console.ReadKey();
+            }
+        }
+    }
+}
