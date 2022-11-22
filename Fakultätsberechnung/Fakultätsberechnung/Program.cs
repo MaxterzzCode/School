6e897dc4a862d60317575788b2a58bcd2d94e8a6
Maximilian Winter <95652868+MaxterzzOnCoding@users.noreply.github.com>
2022-11-22 10:59:19 +0100

    Add files via upload

---
+++ Fakultätsberechnung/Fakultätsberechnung/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Fakultätsberechnung
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int n = 1;
+            int input;
+           
+
+            Console.WriteLine("Fakultaetsberechnung ");
+            Console.WriteLine("");
+            Console.Write("Natuerliche Zahl eingeben ");
+            input = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= input ; i++)
+            {
+                n = n * i;
+               
+                if (i == input) ; {
+                    Console.WriteLine(input + "! = " + n);
+                }
+
+            }
+
+
+            
+        }
+    }
+}
