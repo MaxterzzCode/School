8993e2948d22ab20c812a460b592c4714cb68480
Maximilian Winter <95652868+MaxterzzOnCoding@users.noreply.github.com>
2022-11-22 09:49:31 +0100

    Add files via upload

---
+++ Wuerfel/Wuerfel/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Wuerfel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random wuerf = new Random();
+            int zahl;
+            int zahl1 = 0;
+            zahl = 0;
+            Console.WriteLine("Würfelspiel");
+            Console.WriteLine("===========");
+            do
+            {
+                zahl = wuerf.Next(1, 7);
+                Console.WriteLine("gewürfelte zahl: " + zahl);
+                zahl1 = zahl1 + zahl;
+
+            } while (zahl < 6);
+            Console.WriteLine("");
+            Console.WriteLine("Summe = " + zahl1);
+        }
+    }
+}
