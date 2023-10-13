37da04740746286c2129f6449700beafb9766b95
Maximilian Winter <95652868+MaxterzzCode@users.noreply.github.com>
2023-10-13 11:32:16 +0000

    made test folder

---
+++ test/All/TGI11/CSharp/C# WinForms/Lotto Giotto/Lotto Giotto/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Lotto_Giotto
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+        }
+        bool[] lotto = new bool[50];
+
+
+        private void B_Init_Click(object sender, EventArgs e)
+        {
+            
+            for (int i = 0; i < lotto.Length; i++) {
+                lotto[i] = false;
+            }
+        }
+
+        private void B_AusgabeListBox_Click(object sender, EventArgs e)
+        {
+            LB_Spielfeld.Items.Clear();
+            for (int i = 0; i < lotto.Length; i++)
+            {
+                if (lotto[i] == false)
+                {
+                    LB_Spielfeld.Items.Add("lotto[" + i + "]: " + "O");
+                }
+                else
+                {
+                    LB_Spielfeld.Items.Add("lotto[" + i + "]: " + "X");
+                }
+                
+            }
+        }
+
+        private void B_TestZahlen_Click(object sender, EventArgs e)
+        {
+            lotto[1] = true;
+            lotto[3] = true;
+            lotto[5] = true;
+            lotto[7] = true;
+            lotto[9] = true;
+            lotto[11] = true;
+        }
+
+        private void B_AusgabeLottoZahlen_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < lotto.Length; i++)
+            {
+                if (lotto[i] == true)
+                {
+                    LB_Zahlen.Items.Add(i);
+                }
+            }
+        }
+
+        private void B_Spielen_Click(object sender, EventArgs e)
+        {
+
+            LB_Zahlen.Items.Clear();
+            int anzahl = 0;
+            int zufallszahl = 0;
+            Random rand = new Random();
+
+            while (anzahl < 6)
+            {
+                zufallszahl = rand.Next(1, 50);
+                if (lotto[zufallszahl] == false)
+                {
+                    lotto[zufallszahl] = true;
+                    anzahl++;
+                }
+            }
+            
+
+        }
+    }
+}
---
+++ test/All/TGI11_Redo/CSharp/Quotenzberechnung/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Quotenzberechnung
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double zahl1, zahl2, quotient;
+            while (true)
+            {
+                Console.Write("Was ist die Zahl die Sie teilen wollen?: ");
+                zahl1 = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Was ist der teiler?: ");
+                zahl2 = Convert.ToDouble(Console.ReadLine());
+
+                if (zahl2 != 0)
+                {
+                    quotient = zahl1 / zahl2;
+
+                    Console.WriteLine($"Der Quotient ist {quotient}");
+                    Console.WriteLine("Drücken sie eine beliebige Taste um das Program neu auszuführen.");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Sie dürfen nicht durch 0 teilen!");
+                    Console.WriteLine("Drücken sie eine beliebige Taste um es erneut zu versuchen.");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+
+            }
+        }
+    }
+}
