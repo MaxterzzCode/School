c42603337fad76b9fe2e1c6c0795e60d9863eaa2
Maximilian Winter <95652868+MaxterzzCode@users.noreply.github.com>
2023-10-17 08:26:41 +0200

    Added Schaltjahrberechnung

---
+++ All/TGI11_Redo/CSharp/Schaltjahrberechnung/Schaltjahrberechnung/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Schaltjahrberechnung
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int jahr;
+            bool schaltjahr = false;
+
+            Console.WriteLine("Bitte geben Sie das Jahr ein was berechnet werden soll.");
+            jahr = Convert.ToInt32(Console.Read());
+
+
+            if (jahr % 4 == 0)
+            {
+                schaltjahr = true;
+
+                if (jahr % 100 == 0 && jahr % 400 != 0)
+                {
+                    schaltjahr = false;
+                }
+                 
+            }
+
+
+
+
+            if (schaltjahr == true)
+            {
+                Console.WriteLine($"Das Jahr {jahr} ist ein Schaltjahr");
+            }
+            else
+            {
+                Console.WriteLine($"Das Jahr {jahr} ist kein Schaltjahr");
+            }
+
+        }
+
+    }
+}
